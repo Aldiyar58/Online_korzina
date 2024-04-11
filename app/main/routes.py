1e934456e6744fde659e02879b037ebf37ddc91c
Алдияр Уалиев <Aldiyar58>
2024-04-11 20:27:24 +0500

    add main page

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,11 @@ from flask import render_template, jsonify, url_for, redirect
 from app.main import bp
 
 
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('singn.html')
+
+
 @bp.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     return render_template('signup.html')
